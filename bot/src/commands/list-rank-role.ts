17109d1b665bc60ae8b28ab11916bfddc79552d3
Jack Wright <28375223+jack3898@users.noreply.github.com>
2024-08-25 18:24:16 +0100

    new slash commands to delete and add rank role mappings

--- bot/src/commands/list-rank-role.ts
+++ bot/src/commands/list-rank-role.ts
@@ -1,13 +1,52 @@
 import { PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
 import type { Command } from "../types.js";
+import { assertBot } from "../utils/assert.js";
+import { listRankRole as listRankRoleDb } from "../services/rank-role-service.js";
+import { template } from "../utils/template.js";
+
+const message = template`There are ${"amount"} role mappings.
+_Only a max of 20 are shown._
+
+${"listItems"}`;
+
+const listItem = template`- <@&${"roleid"}>: rank \`${"min"}\` to \`${"max"}\`, id \`${"id"}\``;
 
 export const listRankRole: Command = {
   definition: new SlashCommandBuilder()
     .setName("list-rank-role")
     .setDescription("Show all the rank-to-role mappings.")
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
-  execute(interaction) {
-    // TODO: Finish off
-    return interaction.reply("Coming soon!");
+  async execute(interaction) {
+    const bot = interaction.client;
+
+    assertBot(bot);
+
+    if (!interaction.isChatInputCommand() || !interaction.guildId) {
+      return;
+    }
+
+    const rankRoles = await listRankRoleDb(bot.db, interaction.guildId);
+
+    if (!rankRoles.length) {
+      return interaction.reply(
+        "There are no role mappings! Use `/add-rank-role` to create one.",
+      );
+    }
+
+    return interaction.reply(
+      message({
+        amount: rankRoles.length,
+        listItems: rankRoles
+          .map((rankRole) =>
+            listItem({
+              id: rankRole.id,
+              min: rankRole.min_requirement,
+              max: rankRole.max_requirement,
+              roleid: rankRole.role_id,
+            }),
+          )
+          .join("\n"),
+      }),
+    );
   },
 };

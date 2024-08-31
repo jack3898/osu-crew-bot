85459ee4e760610b9675e2cffd347e79c2c4a4fa
Jack Wright <28375223+jack3898@users.noreply.github.com>
2024-09-01 00:29:44 +0100

    set user activity on launch

--- bot/src/events/ready.ts
+++ bot/src/events/ready.ts
@@ -1,5 +1,8 @@
-import type { Client } from "discord.js";
+import { ActivityType, type Client } from "discord.js";
 
 export function handleReady(bot: Client): void {
   console.log(`Logged in as ${bot.user?.username}!`);
+
+  // It's the meta
+  bot.user?.setActivity("osu!", { type: ActivityType.Playing });
 }

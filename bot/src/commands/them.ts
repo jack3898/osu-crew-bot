affdc1eee3f5aa45264600b701649d1e1be331da
Jack Wright <28375223+jack3898@users.noreply.github.com>
2024-09-03 18:25:18 +0100

    improve /them by fetching discord account linked osu profiles

--- bot/src/commands/them.ts
+++ bot/src/commands/them.ts
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from "discord.js";
 import type { Command } from "../types.js";
 import { assertBot } from "../utils/assert.js";
 import { getOsuApiClient } from "../services/user-service.js";
-import { createUserEmbed } from "../utils/message.js";
+import { createUserEmbed, hide } from "../utils/message.js";
 
 export const them: Command = {
   definition: new SlashCommandBuilder()
@@ -10,9 +10,13 @@ export const them: Command = {
     .setDescription("Not you! Fetch someone else's Osu! profile.")
     .addStringOption((option) =>
       option
-        .setName("username")
-        .setDescription("The Osu! username to fetch.")
-        .setRequired(true),
+        .setName("osu-username")
+        .setDescription("The Osu! username to fetch."),
+    )
+    .addUserOption((option) =>
+      option
+        .setName("discord-user")
+        .setDescription("The Discord user's associated Osu! profile to fetch."),
     ),
   async execute(interaction) {
     const bot = interaction.client;
@@ -23,24 +27,51 @@ export const them: Command = {
       return;
     }
 
-    await interaction.deferReply();
+    const user =
+      interaction.options.getString("osu-username") ??
+      interaction.options.getUser("discord-user");
 
-    const osuClient = await getOsuApiClient(
-      undefined,
-      interaction.user.id,
-      bot.db,
-    );
+    if (typeof user === "string") {
+      await interaction.deferReply();
 
-    if (!osuClient) {
-      return interaction.followUp(
-        "You need to link your Osu! account first. 🥲 Use `/link` to link it!",
+      const osuClient = await getOsuApiClient(
+        undefined,
+        interaction.user.id,
+        bot.db,
       );
+
+      if (!osuClient) {
+        return interaction.followUp(
+          "You need to link your Osu! account first. Use `/link` to link it!",
+        );
+      }
+
+      const clientUser = await osuClient.users.getUser(user);
+      const userEmbed = createUserEmbed(clientUser);
+
+      return interaction.followUp({ embeds: [userEmbed] });
+    } else if (user && typeof user === "object") {
+      await interaction.deferReply();
+
+      const osuClient = await getOsuApiClient(undefined, user.id, bot.db);
+
+      if (!osuClient) {
+        return interaction.followUp(
+          "This user hasn't linked their Osu! account or could not be found.",
+        );
+      }
+
+      const clientUser = await osuClient.users.getSelf();
+
+      const userEmbed = createUserEmbed(clientUser);
+
+      return interaction.followUp({ embeds: [userEmbed] });
     }
 
-    const username = interaction.options.getString("username", true);
-    const user = await osuClient?.users.getUser(username);
-    const userEmbed = createUserEmbed(user);
-
-    await interaction.followUp({ embeds: [userEmbed] });
+    return interaction.reply(
+      hide(
+        "You need to provide a username or a Discord user to fetch their Osu! profile.",
+      ),
+    );
   },
 };

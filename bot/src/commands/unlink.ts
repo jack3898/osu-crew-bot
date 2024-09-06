50386645541ba428bef93a374435c5b5ed69c1f5
Jack Wright <28375223+jack3898@users.noreply.github.com>
2024-09-06 20:02:28 +0100

    disable unlink button after 60s

--- bot/src/commands/unlink.ts
+++ bot/src/commands/unlink.ts
@@ -8,13 +8,24 @@ import {
 import { ButtonId, type Command } from "../types.js";
 import { assertBot } from "../utils/assert.js";
 import { hide } from "../utils/message.js";
+import { setTimeout as sleep } from "timers/promises";
 
 const button = new ButtonBuilder()
   .setCustomId(ButtonId.UnlinkConfirm)
   .setLabel("I am sure")
   .setStyle(ButtonStyle.Danger);
 
+const disabledButton = new ButtonBuilder()
+  .setCustomId(ButtonId.Disabled)
+  .setLabel("Expired")
+  .setStyle(ButtonStyle.Danger)
+  .setDisabled(true);
+
 const actionRow = new ActionRowBuilder<ButtonBuilder>().addComponents(button);
+
+const disabledRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+  disabledButton,
+);
 
 export const unlink: Command = {
   definition: new SlashCommandBuilder()
@@ -31,5 +42,9 @@ export const unlink: Command = {
       ),
       components: [actionRow],
     });
+
+    await sleep(60_000);
+
+    await interaction.editReply({ components: [disabledRow] });
   },
 };
